a86216584c7f9d3695725d05b991b09c773e3986
PRAJWAL1758 <119925088+PRAJWAL1758@users.noreply.github.com>
2024-02-06 18:21:03 +0530

    Update script.js

--- Box Switcher/script.js
+++ Box Switcher/script.js
@@ -3,7 +3,7 @@ const  testimonial = document.querySelector('.testimonial')
 const  userImage = document.querySelector('.user-image')
 const  username = document.querySelector('.username')
 const  role = document.querySelector('.role')
-
+ 
 const testimonials = [
     {
       name: 'Teresa Smith',
