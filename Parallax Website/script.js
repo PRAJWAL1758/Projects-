30a77fa9d7ff7436b2768ef363332672744d0007
PRAJWAL1758 <119925088+PRAJWAL1758@users.noreply.github.com>
2024-02-26 23:57:08 +0530

    Update script.js

--- Parallax Website/script.js
+++ Parallax Website/script.js
@@ -8,7 +8,7 @@ timeline
   .to(".content", 10, { top: "0%" }, "-=10")
   .fromTo(".content-images", { opacity: 0 }, { opacity: 1, duration: 3 })
   .fromTo(".text", { opacity: 0 }, { opacity: 1, duration: 3 });
-
+ 
 let scene = new ScrollMagic.Scene({
   triggerElement: "section",
   duration: "300%",
